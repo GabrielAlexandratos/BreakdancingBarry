d3cc2ed1613e7a3552cb2f106c31a3ccc8d41ae6
Gabriel <gabriel.alexandratos@education.nsw.gov.au>
2025-08-26 21:11:42 +1000

    started main menu navigation

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -3,6 +3,8 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
 
 class TitleState extends FlxState {
@@ -10,6 +12,8 @@ class TitleState extends FlxState {
 	private var TitleLogo:FlxSprite;
     private var Background:FlxSprite;
     private var BackgroundColor:FlxSprite;
+	private var menuOptions:Array<FlxSprite>;
+	private var selectedIndex:Int;
 
 	override public function create() {
 
@@ -33,11 +37,80 @@ class TitleState extends FlxState {
         TitleLogo.scale.set(0.3, 0.3);
 		TitleLogo.screenCenter();
 		add(TitleLogo);
+		menuOptions = [];
+		selectedIndex = 0;
+
+		var startOption = new FlxSprite(0, 0);
+		startOption.loadGraphic("assets/images/mainmenu/play.png", false);
+		startOption.scale.set(0.5, 0.5);
+		add(startOption);
+		menuOptions.push(startOption);
+
+		var optionsOption = new FlxSprite(0, 0);
+		optionsOption.loadGraphic("assets/images/mainmenu/options.png", false);
+		optionsOption.scale.set(0.5, 0.5);
+		add(optionsOption);
+		menuOptions.push(optionsOption);
+
+		var exitOption = new FlxSprite(0, 0);
+		exitOption.loadGraphic("assets/images/mainmenu/exit.png", false);
+		exitOption.scale.set(0.5, 0.5);
+		add(exitOption);
+		menuOptions.push(exitOption);
+
+		positionMenu();
 	}
 
 	override public function update(elapsed:Float) {
 
 		super.update(elapsed);
+		if (FlxG.keys.justPressed.W)
+		{
+			selectedIndex = Std.int(Math.max(0, selectedIndex - 1));
+			positionMenu();
+		}
+		else if (FlxG.keys.justPressed.S)
+		{
+			selectedIndex = Std.int(Math.min(menuOptions.length - 1, selectedIndex + 1));
+			positionMenu();
+		}
+	}
+
+	private function positionMenu():Void
+	{
+		var spacing = 100;
+		for (i in 0...menuOptions.length)
+		{
+			var option = menuOptions[i];
+			var targetY = FlxG.height / 2 + (i - selectedIndex) * spacing;
+			FlxTween.tween(option, {y: targetY}, 0.3, {ease: FlxEase.quadOut});
+
+			if (i == selectedIndex)
+			{
+				option.loadGraphic("assets/images/mainmenu/" + getOptionName(i) + "_selected.png", false);
+			}
+			else
+			{
+				option.loadGraphic("assets/images/mainmenu/" + getOptionName(i) + ".png", false);
+			}
+			option.origin.set(option.width / 2, option.height / 2);
+			option.x = FlxG.width / 2;
+		}
+	}
+
+	private function getOptionName(index:Int):String
+	{
+		switch (index)
+		{
+			case 0:
+				return "start";
+			case 1:
+				return "options";
+			case 2:
+				return "exit";
+			default:
+				return "start";
+		}
 	}
 
 	private function SwitchScene(timer:FlxTimer):Void {
