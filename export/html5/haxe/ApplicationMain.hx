6847c07ed8a3685861a0643acf7a16b417754de8
Gabriel <gabriel.alexandratos@education.nsw.gov.au>
2025-11-03 10:45:09 +1100

    staged for branch

--- export/html5/haxe/ApplicationMain.hx
+++ export/html5/haxe/ApplicationMain.hx
@@ -35,7 +35,7 @@ class ApplicationMain
 		ManifestResources.init(config);
 		#end
 
-		app.meta["build"] = "61";
+		app.meta["build"] = "62";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "BreakdancingBarry";
 		app.meta["name"] = "Beat Boxing Barry";
